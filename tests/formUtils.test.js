fa4d63aca8d57660cd96a06ae5f36da9b0b30045
Brayden Williams <1311325+redstar504@users.noreply.github.com>
2024-01-05 06:30:52 -0800

    Fix broken tests

--- tests/formUtils.test.js
+++ tests/formUtils.test.js
@@ -2,41 +2,41 @@ import {expect, test} from "vitest";
 import * as util from "../src/formUtils.js";
 
 test('should return false for empty name', () => {
-    expect(util.isFullNameValid("")).toBe(false);
+    expect(util.validFullName("")).toBe(false);
 });
 
 test('should return true for a valid name', () => {
-    expect(util.isFullNameValid("foobar")).toBe(true);
+    expect(util.validFullName("foobar")).toBe(true);
 });
 
 test('should return false for a short name', () => {
-    expect(util.isFullNameValid("foo")).toBe(false);
+    expect(util.validFullName("foo")).toBe(false);
 });
 
 test('valid email should be accepted', () => {
-    expect(util.isEmailValid("foo@bar.com")).toBe(true);
+    expect(util.validEmail("foo@bar.com")).toBe(true);
 });
 
 test('invalid email should be denied', () => {
-    expect(util.isEmailValid("foobar.com")).toBe(false);
+    expect(util.validEmail("foobar.com")).toBe(false);
 });
 
 test('valid phone should be accepted', () => {
-    expect(util.isPhoneValid("5554446655")).toBe(true);
+    expect(util.validPhone("5554446655")).toBe(true);
 });
 
 test('phone with dashes should be accepted', () => {
-    expect(util.isPhoneValid("555-444-6655")).toBe(true);
+    expect(util.validPhone("555-444-6655")).toBe(true);
 });
 
 test('phone with parens should be accepted', () => {
-    expect(util.isPhoneValid("(555) 444-6655")).toBe(true);
+    expect(util.validPhone("(555) 444-6655")).toBe(true);
 });
 
 test('phone with spaces should be accepted', () => {
-    expect(util.isPhoneValid("555 444 6655")).toBe(true);
+    expect(util.validPhone("555 444 6655")).toBe(true);
 });
 
 test('invalid phone should be denied', () => {
-    expect(util.isPhoneValid("a55im0v777")).toBe(false);
+    expect(util.validPhone("a55im0v777")).toBe(false);
 });
