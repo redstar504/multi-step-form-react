8ddef2abb8932f0deab998cbe72c8d0d4620b524
Brayden Williams <1311325+redstar504@users.noreply.github.com>
2024-01-06 01:16:29 -0800

    Add some info page UI tests

--- tests/pages/InfoPage.js
+++ tests/pages/InfoPage.js
@@ -1,23 +1,44 @@
 export default class InfoPage {
     constructor(page) {
         this.page = page;
-        this.port = 5173;
+        this.port = 3000;
     }
 
     async open() {
         await this.page.goto(`http://localhost:${this.port}`);
     }
 
-    async fillFullName(fullName) {
-        await this.page.locator("#fullName").fill(fullName);
+    async fillFullName(fullName, blur = false) {
+        const locator = await this.page.locator("#fullName");
+        await locator.fill(fullName);
+        if (blur) await locator.blur();
     }
 
-    async fillEmailAddress(emailAddress) {
-        await this.page.locator("#emailAddress").fill(emailAddress);
+    async fillEmailAddress(emailAddress, blur = false) {
+        const locator = await this.page.locator("#emailAddress");
+        await locator.fill(emailAddress);
+        if (blur) await locator.blur();
     }
 
-    async fillPhoneNumber(phoneNumber) {
-        await this.page.locator("#phoneNumber").fill(phoneNumber);
+    async fillPhoneNumber(phoneNumber, blur = false) {
+        const locator = await this.page.locator("#phoneNumber");
+        await locator.fill(phoneNumber);
+        if (blur) await locator.blur();
+    }
+
+    async hasFirstNameError() {
+        const label = await this.page.locator("label[for='fullName']");
+        return label.evaluate(node => node.className === "error");
+    }
+
+    async hasEmailAddressError() {
+        const label = await this.page.locator("label[for='emailAddress']");
+        return label.evaluate(node => node.className === "error");
+    }
+
+    async hasPhoneNumberError() {
+        const label = await this.page.locator("label[for='phoneNumber']");
+        return label.evaluate(node => node.className === "error");
     }
 
     async isNextStepEnabled() {
--- tests/ui/infoStep.test.js
+++ tests/ui/infoStep.test.js
@@ -1,9 +1,9 @@
 import {afterAll, beforeAll, describe, test} from "vitest";
-import {preview} from "vite";
+import {build, preview} from "vite";
 import InfoPage from "../pages/InfoPage.js";
 import {chromium, expect} from "../runner.js";
 
-const PORT = 5173;
+const PORT = 3000;
 const FULL_NAME = "Benji Simon";
 const EMAIL_ADDRESS = "benji@example.com";
 const PHONE_NUMBER = "5554443232";
@@ -14,8 +14,9 @@ describe("infoStep", async () => {
     let page;
 
     beforeAll(async () => {
+        await build();
         server = await preview({preview: {port: PORT}});
-        browser = await chromium.launch({headless: false});
+        browser = await chromium.launch();
         page = await browser.newPage();
     });
 
@@ -34,5 +35,35 @@ describe("infoStep", async () => {
         await infoPage.fillEmailAddress(EMAIL_ADDRESS);
         await infoPage.fillPhoneNumber(PHONE_NUMBER);
         await expect(await infoPage.isNextStepEnabled()).toBeTruthy();
+    }, 60_000);
+
+    test("error should be functional on invalid first name", async () => {
+        const infoPage = new InfoPage(page);
+        await infoPage.open();
+        await expect(await infoPage.hasFirstNameError()).toBe(false);
+        await infoPage.fillFullName("pork", true);
+        await expect(await infoPage.hasFirstNameError()).toBe(true);
+        await infoPage.fillFullName("porky thepig", true);
+        await expect(await infoPage.hasFirstNameError()).toBeFalsy();
+    }, 60_000);
+
+    test("error should be functional on invalid email address", async () => {
+        const infoPage = new InfoPage(page);
+        await infoPage.open();
+        await expect(await infoPage.hasEmailAddressError()).toBe(false);
+        await infoPage.fillEmailAddress("bob", true);
+        await expect(await infoPage.hasEmailAddressError()).toBe(true);
+        await infoPage.fillEmailAddress("bob.the.builder@example.com", true);
+        await expect(await infoPage.hasEmailAddressError()).toBeFalsy();
+    }, 60_000);
+
+    test("error should be functional on invalid phone number", async () => {
+        const infoPage = new InfoPage(page);
+        await infoPage.open();
+        await expect(await infoPage.hasPhoneNumberError()).toBe(false);
+        await infoPage.fillPhoneNumber("555", true);
+        await expect(await infoPage.hasPhoneNumberError()).toBe(true);
+        await infoPage.fillPhoneNumber("5551234567", true);
+        await expect(await infoPage.hasPhoneNumberError()).toBeFalsy();
     }, 60_000);
 });
