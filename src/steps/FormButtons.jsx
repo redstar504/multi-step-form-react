1c41f0d7b0642103423b1f4b925bbf27d6af78ab
Brayden Williams <1311325+redstar504@users.noreply.github.com>
2024-01-07 08:57:44 -0800

    Hotfix for navigation issue

--- src/steps/FormButtons.jsx
+++ src/steps/FormButtons.jsx
@@ -1,12 +1,16 @@
+import { useNavigate } from 'react-router-dom'
+
 export default function FormButtons({
   forForm,
   onSubmit,
   goBack = null,
   nextLabel = 'Next Step'
 }) {
+  const navigate = useNavigate()
+
   return (
     <footer>
-      {goBack && <a href={goBack} id="prevStep">Go Back</a>}
+      {goBack && <a href="#" onClick={() => navigate(goBack)} id="prevStep">Go Back</a>}
       <button id="nextStep" type="submit" form={forForm} onClick={onSubmit}>{nextLabel}</button>
     </footer>
   )
